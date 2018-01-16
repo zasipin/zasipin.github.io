9019d7e8d582798996c1ba427316957fff7251bf
zasipin <ZasypinNV@yanos.slavneft.ru>
2018-01-16 13:44:41 +0300

    added extra payment inputs

--- loancalculator/sw.js
+++ loancalculator/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var staticCacheName = 'crcalc-static-v4.7.7';
+var staticCacheName = 'crcalc-static-v4.7.8';
 var contentImgsCache = 'crcalc-content-imgs';
 var firstPage = '/mortage';
 var allCaches = [staticCacheName, contentImgsCache];
